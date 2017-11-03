6a1e864f31f6ac77bde7c77b83bb38c88fa7c9fa
catherine Bose <catherinebose@catherines-MacBook-Pro.local>
2017-11-03 15:23:30 -0700

    counter templates added

--- counter/server.py
+++ counter/server.py
@@ -1,3 +1,5 @@
+from flask import Flask, render_template, request, redirect,session
+app = Flask(__name__)
 app.secret_key = 'ThisIsSecret'
 
 
@@ -15,16 +17,16 @@ def count_visits(): # session[key] = value
 def count_reloads():
     if 'count' in session:
         session['count'] += 2
-    else:
-        session['count'] = session['count']
+    # else:
+    #     session['count'] = session['count']
     return render_template('reload.html')
 
 @app.route('/reset')
 def reset_counter():
     if 'count' in session:
         session['count'] = 0
-    else:
-        session['count'] = session['count']
+    # else:
+    #     session['count'] = session['count']
     return render_template('reset.html')
 
 
